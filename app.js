541d37d32d873aa533b5d6041832ae21b80eba74
Bernard301094 <bernard30101994@gmail.com>
2025-01-29 15:42:18 -0300

    Agregar funcionalidad para añadir amigos a la lista y limpiar el campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigos = [];
+let amigos = document.querySelector('#amigo');
+let listaAmigos = document.querySelector('#listaAmigos');
+
+const adicionarAmigo = () => {
+    nomeAmigos.push(amigos.value);
+    listaAmigos.innerHTML += `<li>${amigos.value}</li>`;
+
+    limparBarraAmigos();
+}
+
+
+const limparBarraAmigos = () => {
+    amigos.value = '';
+}
